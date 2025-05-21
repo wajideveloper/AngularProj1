fb63a2bc9232bfe4b449870299d24cdb48260ab9
wajideveloper <wajidullah42540@gmail.com>
2025-05-20 21:00:59 -0700

    this is my first Angular Proj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'OFC_dashboardAng';
+  title = 'To the Angular World!';
 }
