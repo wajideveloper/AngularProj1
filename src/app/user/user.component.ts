4622757121ff813f66b050db1d2ac1730c157b5d
wajideveloper <wajidullah42540@gmail.com>
2025-06-14 18:09:00 +0500

    i have add new changes like how to use dynamic data handling

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
 
- constructor() {  
-  console.log('UserComponent initialized');
- }
+  get imagePath(): string {
+    return `assets/users/${this.selectedUser.avatar}`;
+  }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
+    // console.log('selectedUser clicked');
+  }
 }
